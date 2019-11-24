14883dee285fdd980ce0ba418d0f951f7dda6cfc
Kognise <felix.mattick@gmail.com>
2019-11-23 20:19:31 -0500

    Clickable OG embeds

--- components/og.js
+++ components/og.js
@@ -7,18 +7,31 @@ const fetcher = async (api, url) => {
   return js
 }
 
-export default ({ url }) => {
+export default ({ url, className }) => {
   const { data } = useSWR([ '/api/og', url ], fetcher)
   
   return data ? (
-    <div className='bg-dark-3 rounded pb-3'>
+    <a className={`
+      bg-dark-3 rounded pb-3 block hover:up transitions
+      focus:outline-none focus:shadow-outline
+      overflow-hidden
+      ${className || ''}
+    `} href={data.url} target='_blank' rel='noopener noreferrer'>
       {data.image && <img src={data.image.url} className='rounded-t w-full' />}
-      {data.title && <h3 className='px-3 mt-3 font-bold text-light-1'>{data.title}</h3>}
-      {data.description && (
-        <TruncateMarkup lines={3}>
-          <p className='px-3 mt-1 text-light-3 text-sm'>{data.description}</p>
-        </TruncateMarkup>
+      {data.title && (
+        <div className='px-3 mt-3'>
+          <TruncateMarkup>
+            <h3 className='font-bold text-light-1'>{data.title}</h3>
+          </TruncateMarkup>
+        </div>
       )}
-    </div>
+      {data.description && (
+        <div className='px-3 mt-1'>
+          <TruncateMarkup lines={3}>
+            <p className='text-light-3 text-sm'>{data.description}</p>
+          </TruncateMarkup>
+        </div>
+      )}
+    </a>
   ) : <div className='h-32 bg-dark-3 rounded loading' />
 }
